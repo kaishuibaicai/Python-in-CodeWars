18d7c9bf9472a8b075a6900b07b535eb492c0133
徐梓琅 <272251416@qq.com>
2017-08-31 22:32:00 +0800

    update

--- softwareTestLearning/baidu.py
+++ softwareTestLearning/baidu.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get("http://www.baidu.com")
+driver.get("https://www.baidu.com")
 
 driver.find_element_by_id("kw").send_keys("Selenium2")
 driver.find_element_by_id("su").click()
