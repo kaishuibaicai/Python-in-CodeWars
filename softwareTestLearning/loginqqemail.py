a05a816dadb574e9687b6b68d9c2bc9ded1c324e
徐梓琅 <272251416@qq.com>
2017-09-04 22:19:07 +0800

    something changed

--- softwareTestLearning/loginqqemail.py
+++ softwareTestLearning/loginqqemail.py
@@ -1,12 +1,14 @@
 #coding=utf-8
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get("http://mail.qq.com")
 
 print ('Before login===========')
-
+driver.implicitly_wait(10)
 print (driver.title)
 print (driver.current_url)
 
