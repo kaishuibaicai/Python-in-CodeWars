d66d00480ad922f12514d1a6de86b7de43de095c
徐梓琅 <272251416@qq.com>
2017-09-04 21:19:08 +0800

    add waittest

--- softwareTestLearning/loginqqemail.py
+++ softwareTestLearning/loginqqemail.py
@@ -10,5 +10,5 @@ print ('Before login===========')
 print (driver.title)
 print (driver.current_url)
 
-driver.switch_to.frame(driver.find_element_by_id("login_frame")).find_element_by_id("u").clear()
-driver.switch_to.frame(driver.find_element_by_id("login_frame")).find_element_by_id("u").send_keys("272251416")
+driver.switch_to.frame(driver.find_element_by_id("login_frame")).find_element_by_name("u").clear()
+driver.switch_to.frame(driver.find_element_by_id("login_frame")).find_element_by_name("u").send_keys("272251416")
